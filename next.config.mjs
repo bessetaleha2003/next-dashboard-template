71da7e06c67c9ae6a8c6131ae08165a8bf8dd229
unknown <105841109222@student.unismuh.ac.id>
2024-08-25 19:03:50 +0800

    Selesai Chapter 10

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        ppr: 'incremental',
-      },
+    // experimental: {
+    //     ppr: 'incremental',
+    //   },
     };
-
 export default nextConfig;
